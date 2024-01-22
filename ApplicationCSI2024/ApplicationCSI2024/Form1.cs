fd35632a9c29c9b4f16d695ebee5c062ed7b4c0e
L2csi <l2csiisc2024@gmail.com>
2024-01-22 16:20:53 +0200

    Salut

--- ApplicationCSI2024/ApplicationCSI2024/Form1.cs
+++ ApplicationCSI2024/ApplicationCSI2024/Form1.cs
@@ -16,5 +16,15 @@ namespace ApplicationCSI2024
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bonjour SALAMA");
+        }
     }
 }
